567b753c6926772f220839272f6cac810eee61bd
AndreyIotko <andrey.iotko@bk.ru>
2022-03-01 19:03:09 +0300

    implemented detailed display of the post and its author

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from blog.views import ListPostsView, CreatePostView, UpdatePostView, DeletePostView, ListPostUserView
+from blog.views import ListPostsView, CreatePostView, UpdatePostView, DeletePostView, ListPostUserView, DetailPostView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', ListPostsView.as_view(), name='posts'),
+    path('read_post/<slug:slug>', DetailPostView.as_view(), name='read_post'),
+    path('my_posts/', ListPostUserView.as_view(), name='posts_user'),
     path('create_post/', CreatePostView.as_view(), name='create_post'),
     path('update_post/<slug:slug>', UpdatePostView.as_view(), name='update_post'),
     path('delete_post/<slug:slug>', DeletePostView.as_view(), name='delete_post'),
-    path('my_posts/', ListPostUserView.as_view(), name='posts_user'),
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,37 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from blog.forms import CreateUpdatePost
 from blog.models import Posts
+from users.models import AdvUser
 
 
 class ListPostsView(ListView):
     template_name = 'blog/posts.html'
-    paginate_by = 1
+    paginate_by = 4
 
     def get_queryset(self):
         return Posts.objects.filter(active=True, featured=True)
 
 
-class ListPostUserView(ListView):
+class DetailPostView(DetailView):
+    template_name = 'blog/read_post.html'
+
+    def get_object(self, queryset=None):
+        return Posts.objects.select_related('author').get(slug=self.kwargs['slug'])
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(DetailPostView, self).get_context_data(**kwargs)
+    #     context['post_author'] = AdvUser.objects.get(username=self.object.author)
+    #     return context
+
+
+class ListPostUserView(LoginRequiredMixin, ListView):
     template_name = 'blog/posts_users.html'
 
     def get_queryset(self):
-        return Posts.objects.filter(author=self.request.user, active=True, featured=True)
+        return Posts.objects.filter(author=self.request.user)
 
 
 class CreatePostView(LoginRequiredMixin, CreateView):
